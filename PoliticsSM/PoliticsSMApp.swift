381312e62807179630c33d10ce6fbf0229f9c71a
Adam Januszewski <Adpjan@att.net>
2022-06-30 15:31:26 -0400

    Initial Commit

---
+++ PoliticsSM/PoliticsSMApp.swift
@@ -0,0 +1,17 @@
+//
+//  PoliticsSMApp.swift
+//  PoliticsSM
+//
+//  Created by adam janusewski on 6/30/22.
+//
+
+import SwiftUI
+
+@main
+struct PoliticsSMApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
